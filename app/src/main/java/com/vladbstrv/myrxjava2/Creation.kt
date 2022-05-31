2f7fa607b9a0f5bf4d40dd61a731e0e9c2888144
vlad-bstrv <keocera@icloud.com>
2022-05-31 15:55:45 +0300

    interval

--- app/src/main/java/com/vladbstrv/myrxjava2/Creation.kt
+++ app/src/main/java/com/vladbstrv/myrxjava2/Creation.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class Creation {
 
@@ -15,8 +16,8 @@ class Creation {
     этом либо о каком-либо другом событии подписчика Observer */
     class Producer {
         //Создаем Observable(поток) разными способами
-        fun fromIterable(): Observable<String> {
-            return Observable.fromIterable(listOf("1", "2", "3"))
+        fun interval(): Observable<Long> {
+            return Observable.interval(1, TimeUnit.SECONDS)
         }
     }
 
@@ -24,33 +25,9 @@ class Creation {
     Реализация интерфейса Observer — класс, объект которого лишь «подписывается» на
     Observable */
     class Consumer(private val producer: Producer) {
-        private val stringObserver = object :Observer<String> {
-            var disposable: Disposable? = null
-            override fun onSubscribe(d: Disposable) {
-                disposable = d
-                Log.d(TAG, "onSubscribe() called with: d = $d")
-            }
-
-            override fun onNext(t: String) {
-                Log.d(TAG, "onNext() called with: t = $t")
-            }
-
-            override fun onError(e: Throwable) {
-                Log.d(TAG, "onError() called with: e = ${e.message}")
-            }
-
-            override fun onComplete() {
-                Log.d(TAG, "onComplete() called")
-            }
-
-        }
-
-        fun execJust() {
-            producer.fromIterable().subscribe(stringObserver)
-        }
 
         private fun execLambda() {
-            producer.fromIterable()
+            producer.interval()
                 .subscribe({
                     Log.d(TAG, "onNext: $it")
                 }, {
