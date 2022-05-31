7ea5695934d7af767f5753473a7680715ff71971
vlad-bstrv <keocera@icloud.com>
2022-05-31 15:34:09 +0300

    just

---
+++ app/src/main/java/com/vladbstrv/myrxjava2/Creation.kt
@@ -0,0 +1,71 @@
+package com.vladbstrv.myrxjava2
+
+import android.util.Log
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+
+class Creation {
+
+    fun exec() {
+        Consumer(Producer()).exec()
+    }
+
+    /**Observable «генерирует» некоторый поток данных и уведомляет об
+    этом либо о каком-либо другом событии подписчика Observer */
+    class Producer {
+        //Создаем Observable(поток) разными способами
+        fun just(): Observable<String> {
+            return Observable.just("1", "2", "3")
+        }
+    }
+
+    /**Observer — интерфейс.
+    Реализация интерфейса Observer — класс, объект которого лишь «подписывается» на
+    Observable */
+    class Consumer(private val producer: Producer) {
+        private val stringObserver = object :Observer<String> {
+            var disposable: Disposable? = null
+            override fun onSubscribe(d: Disposable) {
+                disposable = d
+                Log.d(TAG, "onSubscribe() called with: d = $d")
+            }
+
+            override fun onNext(t: String) {
+                Log.d(TAG, "onNext() called with: t = $t")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG, "onError() called with: e = ${e.message}")
+            }
+
+            override fun onComplete() {
+                Log.d(TAG, "onComplete() called")
+            }
+
+        }
+
+        fun execJust() {
+            producer.just().subscribe(stringObserver)
+        }
+
+        private fun execLambda() {
+            producer.just()
+                .subscribe({
+                    Log.d(TAG, "onNext: $it")
+                }, {
+                    Log.d(TAG, "onError: ${it.message}")
+                }, {
+                    Log.d(TAG, "onComplete")
+                })
+        }
+        //подписываемся на Observable
+        fun exec() {
+            execLambda()
+        }
+    }
+
+    companion object {
+        const val TAG = "TAG"
+    }
+}
